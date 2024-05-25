776d1468cc43cdf16e342179b7e6a4ec0b98f1dd
Mustafa Adnan ŞENER <96550499+adnansener@users.noreply.github.com>
2024-05-26 02:52:08 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -60,10 +60,17 @@ CREATE TABLE `buy_history` (
 -- INSERT SOME VALUES INTO OUR TABLES
 
 INSERT INTO `product` (`product_id`, `product_name`, `product_author`, `product_quantity`, `product_photo`) VALUES
-(1, 'fareler ve insanlar', 'john steinbeck', 4, 'myImages/farelerveinsanlar.jpg'),
-(2, 'inci', 'john steinbeck', 3, 'myImages/inci.jpg'),
-(3, 'On Sözcükte Çin', 'Yu Hua', 5, 'myImages/onsozcuktecin.jpg'),
-(4, 'Yaşamak', 'Yu Hua', 0, 'myImages/yasamak.jpg'),
-(5, 'Kırmızı Papağan', 'jose Mauro De Vasconcelos', 1, 'myImages/kirmizipapagan.jpg'),
-(6, 'Şeker Portakalı', 'jose Mauro De Vasconcelos', 6, 'myImages/sekerportakali.jpg'),
-(7, 'Momo', 'Michael Ende', 5, 'myImages/momo.jpg');
+(1, 'A Game of Thrones', 'George R. R. Martin', 4, 'myImages/a_game_of_thrones.png'),
+(2, 'A Feast for Crows', 'George R. R. Martin', 3, 'myImages/a_feast_for_crows.png'),
+(3, 'A Dance With Dragons', 'George R. R. Martin', 5, 'myImages/a_dance_with_dragons.png'),
+(4, 'A Storm of Swords', 'George R. R. Martin', 0, 'myImages/a_storm_of_swords.png'),
+(5, 'The Hobbit', 'J. R. R. Tolkien', 1, 'myImages/the_hobbit.png'),
+(6, 'Unfinished Tales', 'J. R. R. Tolkien', 6, 'myImages/unfinished_tales.png'),
+(7, 'The Lord of the Rings', 'J. R. R. Tolkien', 1, 'myImages/the_lord_of_the_rings.png'),
+(8, 'Harry Potter and the Philosophers Stone', 'J. K. Rowling', 21, 'myImages/harry_potter_and_the_philosophers_stone.png'),
+(9, 'Harry Potter and the Chamber of Secrets', 'J. K. Rowling', 3, 'myImages/harry_potter_and_the_chamber_of_secrets.png'),
+(10, 'Harry Potter and the Prisoner of Azkaban', 'J. K. Rowling', 6, 'myImages/harry_potter_and_the_prisoner_of_azkaban.png'),
+(11, 'Harry Potter and the Goblet of Fire', 'J. K. Rowling', 2, 'myImages/harry_potter_and_the_goblet_of_fire.png'),
+(12, 'Harry Potter and the Order of the Phoenix', 'J. K. Rowling', 0, 'myImages/harry_potter_and_the_order_of_the_phoenix.png'),
+(13, 'Harry Potter and the Half Blood Prince', 'J. K. Rowling', 3, 'myImages/harry_potter_and_the_half_blood_prince.png'),
+(14, 'Harry Potter and the Deathly Hallows', 'J. K. Rowling', 4, 'myImages/harry_potter_and_the_deathly_hallows.png');
